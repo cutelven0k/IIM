94e2b7aa48732143362922002c3cd07f319f1b9a
mrnavruzov <mrnavruzov@edu.hse.ru>
2025-03-28 00:11:56 +0300

    Moved scripts to dir scripts

---
+++ scripts/apply_migrations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DB_URL="postgresql://postgres:qwerty@localhost:5436/postgres"
+MIGRATIONS_DIR="schemas/postgresql/auth/migrations"
+MIGRATIONS_LIST="schemas/postgresql/auth/migrations.txt"
+
+while read -r migration; do
+    echo "Applying migration: $MIGRATIONS_DIR/$migration"
+    psql "$DB_URL" -f "$MIGRATIONS_DIR/$migration"
+done < "$MIGRATIONS_LIST"
+
+echo "All migrations applied."
