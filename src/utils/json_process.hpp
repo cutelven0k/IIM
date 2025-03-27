b3445fd344d451c02a219504b81f4cc37f12399e
mrnavruzov <mrnavruzov@edu.hse.ru>
2025-03-28 00:10:18 +0300

    Added util for creating responses in json foramt for handler functions and added util for processing json

---
+++ src/utils/json_process.hpp
@@ -0,0 +1,4 @@
+#include <userver/formats/json/value.hpp>
+#include <userver/server/http/http_request.hpp>
+
+userver::formats::json::Value GetJsonBodyFromRequest(const userver::server::http::HttpRequest& request);
---
+++ src/utils/string_json_response.hpp
@@ -0,0 +1,4 @@
+#include <string>
+
+std::string MessageResponse(const std::string& message);
+std::string ErrorResponse(const std::string& error);
